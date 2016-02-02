e6d4f5e3e46656692efccda264fc9c0917948d7c
galkow <orian@mail.sapir.ac.il>
2016-02-02 17:57:14 +0200

    data extraction added

--- carder.py
+++ carder.py
@@ -3,13 +3,17 @@ __author__ = 'orian'
 from selenium import webdriver
 import requests
 import time
+import xml.etree.cElementTree as ET
 
 
 
 
 class Spider():
 
-    __phantom = webdriver.PhantomJS(executable_path='C:/Users/orian/PycharmProjects/Penny/node_modules/phantomjs/bin/phantomjs.exe')
+    #__phantom = webdriver.PhantomJS(executable_path='C:/Users/orian/PycharmProjects/Penny/node_modules/phantomjs/bin/phantomjs.exe')
+    __phantom = webdriver.Firefox()
+    page = 0
+
 
     def extractCaptch(self):
             xpath = '//*[@id="cards"]/div[2]/table/tbody/tr/td/table/tbody/tr[3]/td[1]/img'
@@ -20,8 +24,8 @@ class Spider():
 
             location = self.__phantom.find_element_by_xpath(xpath)
             src = location.get_attribute('src')
-            print ("captcha img url: ")
-            print (src)
+            print("captcha img url: ")
+            print(src)
 
             with open('captcha.jpg', 'wb') as handle:
                 response = requests.get(src, stream=True)
@@ -38,7 +42,7 @@ class Spider():
     def login(self):
 
         captcha = input("Captcha? ")
-        print ("Captcha entered: "+ captcha)
+        print("Captcha entered: " + captcha)
 
         carder_username = "sinistar"
         carder_password = "coolice#9"
@@ -60,9 +64,27 @@ class Spider():
         captchFieldElement.send_keys(captcha)
 
         loginButton.click()
-        time.sleep(5)
 
-        print (self.__phantom.page_source)
+
+    def navigate(self):
+        last_page_xpath = '//*[@id="cards"]/div[2]/a[2]'
+        url = "https://carder007.org/cards.php?category_id=4&stagnant=&txtBin=&page=" + str(self.page)
+        self.__phantom.get(url)
+        last_page = self.__phantom.find_element_by_xpath(last_page_xpath).text
+        return last_page
+
+    def table_traverse(self, page):
+
+        url = "https://carder007.org/cards.php?category_id=4&stagnant=&txtBin=&page=" + str(page)
+        self.__phantom.get(url)
+        data = []
+        for tr in self.__phantom.find_elements_by_xpath('//table[@class="content_table borderstyle td_border"]//tr'):
+            tds = tr.find_elements_by_tag_name('td')
+            if tds:
+                data.append([td.text for td in tds])
+
+        print(data)
+
 
 
 
@@ -75,6 +97,11 @@ def main():
     spider = Spider()
     spider.extractCaptch()
     spider.login()
+
+    last_page = int(spider.navigate())
+    for i in range(0, last_page+1):
+        spider.table_traverse(i)
+
 
 
 if __name__=='__main__':
